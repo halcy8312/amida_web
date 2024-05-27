095be111e8b9115dbe009dbfaff6c588ba44bec8
halcy8312 <157008050+halcy8312@users.noreply.github.com>
2024-05-27 19:44:55 +0900

    Retest0.0.9

--- app/__init__.py
+++ app/__init__.py
@@ -2,35 +2,28 @@ from flask import Flask
 from celery import Celery
 from flask_cors import CORS
 import os
+import logging
 
+celery = Celery(__name__, broker=os.getenv('CELERY_BROKER_URL'))
 
 def create_app():
-    celery = Celery(__name__, broker=os.getenv('CELERY_BROKER_URL'))  # Celeryインスタンスを関数内で定義
-
-    app = Flask(__name__, template_folder='../templates')
+    app = Flask(__name__, template_folder='templates')
     app.config['CELERY_BROKER_URL'] = os.getenv('CELERY_BROKER_URL')
     app.config['CELERY_RESULT_BACKEND'] = os.getenv('CELERY_RESULT_BACKEND')
     app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
     CORS(app)
 
+    logging.basicConfig(level=logging.DEBUG)
+    app.logger.debug(f"CELERY_BROKER_URL: {os.getenv('CELERY_BROKER_URL')}")
+    app.logger.debug(f"CELERY_RESULT_BACKEND: {os.getenv('CELERY_RESULT_BACKEND')}")
+    app.logger.debug(f"FLASK_SECRET_KEY: {os.getenv('FLASK_SECRET_KEY')}")
+
     from .routes import main
     app.register_blueprint(main)
 
-    # Celery 設定を Flask アプリケーションのコンテキスト内で行う
-    def make_celery(app):
-        celery.conf.update(app.config)
-        TaskBase = celery.Task
-        class ContextTask(TaskBase):
-            abstract = True
-            def __call__(self, *args, **kwargs):
-                with app.app_context():
-                    return TaskBase.__call__(self, *args, **kwargs)
-        celery.Task = ContextTask
-        return celery
+    celery.conf.update(app.config)
 
-    celery = make_celery(app)
+    with app.app_context():
+        import app.tasks
 
-    # タスクの自動登録を関数内で行う
-    import app.tasks 
-
-    return app, celery  # app と celery の両方を返す
+    return app
--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app
+import logging
 
-app, _ = create_app() 
+app = create_app()
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     app.run(debug=True)
