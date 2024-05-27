2b971e00a7ba5f071367a350e5797517a950e51c
halcy8312 <157008050+halcy8312@users.noreply.github.com>
2024-05-27 17:13:07 +0900

    Retest0.0.3

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from celery import Celery
 from flask_cors import CORS
+import os
 
-celery = Celery(__name__, broker='redis://localhost:6379/0')
+celery = Celery(__name__, broker=os.getenv('CELERY_BROKER_URL'))
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile('../.env')
+    app.config['CELERY_BROKER_URL'] = os.getenv('CELERY_BROKER_URL')
+    app.config['CELERY_RESULT_BACKEND'] = os.getenv('CELERY_RESULT_BACKEND')
+    app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
     CORS(app)
 
     from .routes import main
