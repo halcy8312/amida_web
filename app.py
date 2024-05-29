2a39ed7dc794296094fa722b0ce31ad081a1acba
halcy8312 <157008050+halcy8312@users.noreply.github.com>
2024-05-29 13:59:50 +0900

    大幅見た目変更5

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, jsonify, send_file
+from flask import Flask, request, render_template, jsonify, send_from_directory
 from yt_dlp import YoutubeDL, DownloadError
 import logging
 import os
@@ -19,6 +19,18 @@ if not os.path.exists(app.config['DOWNLOAD_FOLDER']):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
+
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 @app.route('/download', methods=['POST'])
 def download():
@@ -61,11 +73,7 @@ def download():
 
 @app.route('/download_file/<filename>')
 def download_file(filename):
-    file_path = os.path.join(app.config['DOWNLOAD_FOLDER'], filename)
-    if os.path.exists(file_path):
-        return send_file(file_path, as_attachment=True, download_name=filename)
-    else:
-        return jsonify({'error': 'File not found.'}), 404
+    return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
